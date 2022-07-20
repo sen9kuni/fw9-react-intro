c4b68aa9992efa8dbbd9be2ea674cdf749e5d26f
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-07-20 22:20:55 +0700

    code: change to rick and morty pic

--- src/App.js
+++ src/App.js
@@ -1,64 +1,44 @@
+import axios from "axios";
 import React from "react";
-import {Col, Row, Container} from 'react-bootstrap'
+import {Row, Container} from 'react-bootstrap'
 
-// class Series extends React.Component {
-//   render(){
-//     return(
-//       <>
-//       {this.props.children}
-//       <li>{this.props.name}</li>
-//       </>
-//     )
-//   }
-// }
+const api = axios.create({
+  baseURL: 'https://rickandmortyapi.com/api/character'
+})
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <ol>
-//         <Series name="Hello" />
-//       </ol>
-//     )
-//   }
-// }
+class App extends React.Component {
+  state = {
+    gambar: []
+  }
+  constructor(){
+    super();
+    api.get('/').then(res => {
+      console.log(res.data.results); 
+      this.setState({gambar: res.data.results})
+    })
+  }
 
-// export default App
-// const Series = ({name, children}) => {
-//   return (
-//     <>
-//     {children}
-//     <li>{name}</li>
-//     </>
-//   )
-// }
-
-// const App = ()=>{
-//   return (
-//     <ol>
-//       <Series name={'yoga'}>wolololo</Series>
-//     </ol>
-//   )
-// }
-
-// export default App
-
-const App = ()=>{
-  const [num, setNumber] = React.useState(0)
-  return (
-    <Container className="p-5 vh-100">
-        <Row className="h-100 flex align-items-center text-center">
-          <Col className='flex'>
-            <button onClick={()=>setNumber(num-1)} className="btn btn-primary square fs-1">-</button>
-          </Col>
-          <Col>
-            <h1 className="fw-bold fs-1">{num}</h1>
-          </Col>
-          <Col className='flex'>
-            <button onClick={()=>setNumber(num+1)} className="btn btn-danger square fs-1">+</button>
-          </Col>
-        </Row>
-    </Container>
-  )
+  render() {
+    return (
+      <>
+      <Container>
+        <h1 className="text-center fw-bold">Rick and Morty Page 1</h1>
+      <Row>
+      {this.state.gambar.map(gbr =>
+      // <Col className="col-3">
+      // <img src={gbr.image} alt='gambar' className="w-100" />
+      // <h3 className="text-center fw-bold">{gbr.name}</h3>
+      // </Col>
+      <figure className="col-4 col-md-3">
+      <img src={gbr.image} alt='gambar' className="w-100" />
+      <figcaption className="fs-4 fs-md-3 fw-bold text-center">{gbr.name}</figcaption>
+      </figure>
+      )}
+      </Row>
+      </Container>
+      </>
+    )
+  }
 }
 
 export default App
