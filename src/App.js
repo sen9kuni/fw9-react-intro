f100b3d7f7c2c7d43c7ea19a020dbe50f7647752
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-07-20 15:14:16 +0700

    core: add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Col, Row, Container} from 'react-bootstrap'
 
-function App() {
+// class Series extends React.Component {
+//   render(){
+//     return(
+//       <>
+//       {this.props.children}
+//       <li>{this.props.name}</li>
+//       </>
+//     )
+//   }
+// }
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <ol>
+//         <Series name="Hello" />
+//       </ol>
+//     )
+//   }
+// }
+
+// export default App
+// const Series = ({name, children}) => {
+//   return (
+//     <>
+//     {children}
+//     <li>{name}</li>
+//     </>
+//   )
+// }
+
+// const App = ()=>{
+//   return (
+//     <ol>
+//       <Series name={'yoga'}>wolololo</Series>
+//     </ol>
+//   )
+// }
+
+// export default App
+
+const App = ()=>{
+  const [num, setNumber] = React.useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="p-5 vh-100">
+        <Row className="h-100 flex align-items-center text-center">
+          <Col className='flex'>
+            <button onClick={()=>setNumber(num-1)} className="btn btn-primary square fs-1">-</button>
+          </Col>
+          <Col>
+            <h1 className="fw-bold fs-1">{num}</h1>
+          </Col>
+          <Col className='flex'>
+            <button onClick={()=>setNumber(num+1)} className="btn btn-danger square fs-1">+</button>
+          </Col>
+        </Row>
+    </Container>
+  )
 }
 
-export default App;
+export default App
